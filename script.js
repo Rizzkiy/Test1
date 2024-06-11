7dc76b12f86e5672f54b6fbf954be6a56509804c
Rzy <164840679+Rizzkiy@users.noreply.github.com>
2024-06-11 12:15:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function playGame() {
+    alert('Game is starting!');
+    // Add your game start logic here
+}
+
+function pauseGame() {
+    alert('Game is paused!');
+    // Add your game pause logic here
+}
+
+function restartGame() {
+    alert('Game is restarting!');
+    // Add your game restart logic here
+}
