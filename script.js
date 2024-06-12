70fd1d5f83160163a5089b101d42effec29fac39
Rzy <164840679+Rizzkiy@users.noreply.github.com>
2024-06-12 13:58:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-function playGame() {
-    alert('Game is starting!');
-    // Add your game start logic here
-}
-
-function pauseGame() {
-    alert('Game is paused!');
-    // Add your game pause logic here
-}
-
-function restartGame() {
-    alert('Game is restarting!');
-    // Add your game restart logic here
-}
+document.getElementById('learnMoreLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://bnpb.go.id/definisi-bencana'; // Ganti dengan URL tujuan Anda
+});
